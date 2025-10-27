fd9a55d26bfbea9a13c62602bb9cacbecdabe4e4
rapon1kt <shinnatensei@gmail.com>
2025-10-27 17:00:17 -0300

    feat: adicionando configuracoes de segurança ao projeto

---
+++ backend/src/main/java/com/teo/gestor/infrastructure/config/security/SecurityConfig.java
@@ -0,0 +1,84 @@
+package com.teo.gestor.infrastructure.config.security;
+
+import java.util.List;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.authentication.AuthenticationManager;
+import org.springframework.security.authentication.AuthenticationProvider;
+import org.springframework.security.authentication.dao.DaoAuthenticationProvider;
+import org.springframework.security.config.Customizer;
+import org.springframework.security.config.annotation.authentication.configuration.AuthenticationConfiguration;
+import org.springframework.security.config.annotation.method.configuration.EnableMethodSecurity;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.annotation.web.configurers.AbstractHttpConfigurer;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.security.crypto.password.PasswordEncoder;
+import org.springframework.security.web.SecurityFilterChain;
+import org.springframework.security.web.authentication.UsernamePasswordAuthenticationFilter;
+import org.springframework.web.cors.CorsConfiguration;
+import org.springframework.web.cors.CorsConfigurationSource;
+import org.springframework.web.cors.UrlBasedCorsConfigurationSource;
+
+import com.teo.gestor.application.service.AppAccount;
+import com.teo.gestor.infrastructure.config.JWTFilter;
+
+@Configuration
+@EnableWebSecurity
+@EnableMethodSecurity
+public class SecurityConfig {
+
+  private final AppAccount appAccount;
+  private final JWTFilter jwtFilter;
+
+  public SecurityConfig(AppAccount appAccount, JWTFilter jwtFilter) {
+    this.appAccount = appAccount;
+    this.jwtFilter = jwtFilter;
+  }
+
+  public @Bean PasswordEncoder passwordEncoder() {
+    return new BCryptPasswordEncoder();
+  }
+
+  public UserDetailsService userDetailsService() {
+    return appAccount;
+  }
+
+  public @Bean AuthenticationProvider authenticationProvider() {
+    DaoAuthenticationProvider provider = new DaoAuthenticationProvider(appAccount);
+    provider.setPasswordEncoder(passwordEncoder());
+    return provider;
+  }
+
+  public @Bean AuthenticationManager authenticationManager(AuthenticationConfiguration config) throws Exception {
+    return config.getAuthenticationManager();
+  }
+
+  public @Bean CorsConfigurationSource corsConfigurationSource() {
+    CorsConfiguration configuration = new CorsConfiguration();
+    configuration.setAllowedOrigins(List.of("http://localhost:3000"));
+    configuration.setAllowedMethods(List.of("GET", "POST", "PUT", "DELETE"));
+    configuration.setAllowedHeaders(List.of("*"));
+    configuration.setAllowCredentials(true);
+    UrlBasedCorsConfigurationSource source = new UrlBasedCorsConfigurationSource();
+    source.registerCorsConfiguration("/**", configuration);
+    return source;
+  }
+
+  public @Bean SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception {
+    http
+        .csrf(AbstractHttpConfigurer::disable)
+        .cors(Customizer.withDefaults())
+        .authorizeHttpRequests(registry -> {
+          registry.requestMatchers("/req/**").permitAll();
+          registry.anyRequest().authenticated();
+        })
+        .httpBasic(Customizer.withDefaults())
+        .addFilterBefore(this.jwtFilter, UsernamePasswordAuthenticationFilter.class);
+
+    return http.build();
+  }
+
+}
