1bdeb481a70f54ad045b7e1e286cebee6449fdfb
rapon1kt <shinnatensei@gmail.com>
2025-10-27 17:09:35 -0300

    feat: adicionando servico de login implementando pelo caso de uso login

---
+++ backend/src/main/java/com/teo/gestor/application/service/user/LoginUserService.java
@@ -0,0 +1,43 @@
+package com.teo.gestor.application.service.user;
+
+import org.springframework.security.authentication.AuthenticationManager;
+import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
+import org.springframework.security.core.Authentication;
+import org.springframework.stereotype.Service;
+
+import com.teo.gestor.application.service.JWTService;
+import com.teo.gestor.domain.model.User;
+import com.teo.gestor.domain.usecases.LoginUserUseCase;
+import com.teo.gestor.infrastructure.persistence.entity.UserEntity;
+import com.teo.gestor.infrastructure.persistence.repository.UserRepository;
+import com.teo.gestor.presentation.mapper.UserMapper;
+
+@Service
+public class LoginUserService implements LoginUserUseCase {
+
+  private final UserRepository userRepository;
+  private final AuthenticationManager authenticationManager;
+  private final JWTService jwtService;
+  private final UserMapper mapper;
+
+  public LoginUserService(UserRepository userRepository, AuthenticationManager authenticationManager,
+      JWTService jwtService, UserMapper mapper) {
+    this.userRepository = userRepository;
+    this.authenticationManager = authenticationManager;
+    this.jwtService = jwtService;
+    this.mapper = mapper;
+  }
+
+  @Override
+  public String handle(String email, String password) {
+    UserEntity userEntity = this.userRepository.findByEmail(email)
+        .orElseThrow(() -> new IllegalArgumentException("This account cannot be found."));
+    User user = this.mapper.toDomain(userEntity);
+    Authentication authentication = this.authenticationManager
+        .authenticate(new UsernamePasswordAuthenticationToken(user.getName(), password));
+    if (!authentication.isAuthenticated())
+      throw new IllegalArgumentException("This account cannot be found.");
+    return jwtService.generateToken(user);
+  }
+
+}
---
+++ backend/src/main/java/com/teo/gestor/domain/usecases/LoginUserUseCase.java
@@ -0,0 +1,5 @@
+package com.teo.gestor.domain.usecases;
+
+public interface LoginUserUseCase {
+  public String handle(String email, String password);
+}
