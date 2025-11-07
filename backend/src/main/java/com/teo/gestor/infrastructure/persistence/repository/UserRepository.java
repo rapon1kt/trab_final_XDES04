15cc95b2dd9c38008511983352743422f898f35b
rapon1kt <shinnatensei@gmail.com>
2025-11-07 15:07:04 -0300

    feat: adicionando novo método de existência no repositório do usuário

--- backend/src/main/java/com/teo/gestor/infrastructure/persistence/repository/UserRepository.java
+++ backend/src/main/java/com/teo/gestor/infrastructure/persistence/repository/UserRepository.java
@@ -14,4 +14,6 @@ public interface UserRepository extends MongoRepository<UserEntity, String> {
 
   public Optional<UserEntity> findByEmail(String email);
 
+  public boolean existsByEmail(String email);
+
 }
