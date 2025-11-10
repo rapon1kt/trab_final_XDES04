3d6b179821a1934f363f80027fc11b6594841236
rapon1kt <shinnatensei@gmail.com>
2025-11-10 14:24:58 -0300

    feat: implementando serviço no controlador junto com request DTO

--- backend/src/main/java/com/teo/gestor/presentation/controller/UserController.java
+++ backend/src/main/java/com/teo/gestor/presentation/controller/UserController.java
@@ -1,12 +1,15 @@
 package com.teo.gestor.presentation.controller;
 
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.PatchMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+import com.teo.gestor.application.service.user.DeleteUserService;
 import com.teo.gestor.application.service.user.UpdateUserService;
+import com.teo.gestor.presentation.dto.DeleteUserRequestDTO;
 import com.teo.gestor.presentation.dto.UpdateUserRequestDTO;
 
 import org.springframework.web.bind.annotation.RequestParam;
@@ -16,15 +19,23 @@ import org.springframework.web.bind.annotation.RequestParam;
 public class UserController {
 
   private final UpdateUserService updateUserService;
+  private final DeleteUserService deleteUserService;
 
-  public UserController(UpdateUserService updateUserService) {
+  public UserController(UpdateUserService updateUserService, DeleteUserService deleteUserService) {
     this.updateUserService = updateUserService;
+    this.deleteUserService = deleteUserService;
   }
 
   @PatchMapping
   public ResponseEntity<?> update(@RequestParam("id") String userId,
       @RequestBody UpdateUserRequestDTO requestDTO) {
     return ResponseEntity.ok(this.updateUserService.handle(userId, requestDTO.getEmail(), requestDTO.getPassword()));
+  }
+
+  @DeleteMapping
+  public ResponseEntity<?> delete(@RequestParam("id") String userId,
+      @RequestBody DeleteUserRequestDTO requestDTO) {
+    return ResponseEntity.ok(this.deleteUserService.handle(userId, requestDTO.getPassword()));
   }
 
 }
---
+++ backend/src/main/java/com/teo/gestor/presentation/dto/DeleteUserRequestDTO.java
@@ -0,0 +1,18 @@
+package com.teo.gestor.presentation.dto;
+
+import jakarta.validation.constraints.NotBlank;
+
+public class DeleteUserRequestDTO {
+
+  @NotBlank(message = "É obrigatório informar a senha do administrador!")
+  private String password;
+
+  public String getPassword() {
+    return password;
+  }
+
+  public void setPassword(String password) {
+    this.password = password;
+  }
+
+}
