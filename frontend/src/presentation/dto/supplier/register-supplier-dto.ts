87cce5f0e541215c5b788c92d53b73db9d28168c
rapon1kt <shinnatensei@gmail.com>
2025-11-11 16:11:27 -0300

    feat: criando interface de requisicao para o fornecedor

---
+++ frontend/src/presentation/dto/supplier/register-supplier-dto.ts
@@ -0,0 +1,12 @@
+export default interface RegisterSupplierDTO {
+  enterpriseName: string;
+  cnpj: string;
+  phone: string;
+  email: string;
+  cep: string;
+  state: string;
+  city: string;
+  street: string;
+  number: string;
+  district: string;
+}
