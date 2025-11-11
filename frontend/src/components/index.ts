5b9f7c30d897354be3e67276666c3e14a58ad81a
rapon1kt <shinnatensei@gmail.com>
2025-11-11 16:11:08 -0300

    feat: exportando todos os componentes da propria pasta

---
+++ frontend/src/components/index.ts
@@ -0,0 +1,3 @@
+export { default as NavBar } from "./nav-bar/nav-bar";
+export { default as DashboardUser } from "./dashboard-user/dashboard-user";
+export { default as DashboardSupplier } from "./dashboard-supplier/dashboard-supplier";
