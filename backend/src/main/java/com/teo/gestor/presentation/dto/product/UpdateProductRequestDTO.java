4ce89fc840a26e4bd4846c387a00944fa693b1ff
rapon1kt <shinnatensei@gmail.com>
2025-11-13 15:08:59 -0300

    feat: criando dto de requisição para atualizar um produto

---
+++ backend/src/main/java/com/teo/gestor/presentation/dto/product/UpdateProductRequestDTO.java
@@ -0,0 +1,90 @@
+package com.teo.gestor.presentation.dto.product;
+
+import java.math.BigDecimal;
+import java.time.Instant;
+
+public class UpdateProductRequestDTO {
+
+  private String id;
+  private String name;
+  private String description;
+  private String categoryId;
+  private String supplierId;
+  private Number quantity;
+  private BigDecimal buyPrice;
+  private BigDecimal salePrice;
+  private Instant validDate;
+
+  public String getId() {
+    return id;
+  }
+
+  public void setId(String id) {
+    this.id = id;
+  }
+
+  public String getName() {
+    return name;
+  }
+
+  public void setName(String name) {
+    this.name = name;
+  }
+
+  public String getDescription() {
+    return description;
+  }
+
+  public void setDescription(String description) {
+    this.description = description;
+  }
+
+  public String getCategoryId() {
+    return categoryId;
+  }
+
+  public void setCategoryId(String categoryId) {
+    this.categoryId = categoryId;
+  }
+
+  public String getSupplierId() {
+    return supplierId;
+  }
+
+  public void setSupplierId(String supplierId) {
+    this.supplierId = supplierId;
+  }
+
+  public Number getQuantity() {
+    return quantity;
+  }
+
+  public void setQuantity(Number quantity) {
+    this.quantity = quantity;
+  }
+
+  public BigDecimal getBuyPrice() {
+    return buyPrice;
+  }
+
+  public void setBuyPrice(BigDecimal buyPrice) {
+    this.buyPrice = buyPrice;
+  }
+
+  public BigDecimal getSalePrice() {
+    return salePrice;
+  }
+
+  public void setSalePrice(BigDecimal salePrice) {
+    this.salePrice = salePrice;
+  }
+
+  public Instant getValidDate() {
+    return validDate;
+  }
+
+  public void setValidDate(Instant validDate) {
+    this.validDate = validDate;
+  }
+
+}
