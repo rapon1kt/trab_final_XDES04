f3b1b373248863854b5131d8e496fa6ce97c7ee4
rapon1kt <shinnatensei@gmail.com>
2025-11-13 14:55:54 -0300

    feat: criando mapper para o produto

---
+++ backend/src/main/java/com/teo/gestor/presentation/mapper/ProductMapper.java
@@ -0,0 +1,15 @@
+package com.teo.gestor.presentation.mapper;
+
+import org.mapstruct.Mapper;
+
+import com.teo.gestor.domain.model.Product;
+import com.teo.gestor.infrastructure.persistence.entity.ProductEntity;
+
+@Mapper(componentModel = "spring")
+public interface ProductMapper {
+
+  ProductEntity toEntity(Product product);
+
+  Product toDomain(ProductEntity productEntity);
+
+}
