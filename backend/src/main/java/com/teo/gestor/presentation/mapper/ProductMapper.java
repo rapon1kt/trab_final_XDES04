8df4f8e6c8e0580608c77e6cf6c07b30d88c7a9e
rapon1kt <shinnatensei@gmail.com>
2025-11-13 15:08:40 -0300

    feat: implementando metodo de mapeamento de DTO para Model

--- backend/src/main/java/com/teo/gestor/presentation/mapper/ProductMapper.java
+++ backend/src/main/java/com/teo/gestor/presentation/mapper/ProductMapper.java
@@ -4,6 +4,7 @@ import org.mapstruct.Mapper;
 
 import com.teo.gestor.domain.model.Product;
 import com.teo.gestor.infrastructure.persistence.entity.ProductEntity;
+import com.teo.gestor.presentation.dto.product.UpdateProductRequestDTO;
 
 @Mapper(componentModel = "spring")
 public interface ProductMapper {
@@ -11,5 +12,7 @@ public interface ProductMapper {
   ProductEntity toEntity(Product product);
 
   Product toDomain(ProductEntity productEntity);
+
+  Product toUpdatedProduct(UpdateProductRequestDTO requestDTO);
 
 }
